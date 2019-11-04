24b73a37808453ed0bf8e27099837865e1b64fea
kazuma1989 <kazuma.ebina@quipper.com>
2019-11-04 16:47:48 +0900

    chore: Reduce configs

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
-    setTimeout(() => setCount(v => v + 1), 1000)
-  })
+    setTimeout(() => setCount(v => v + 1), 1000);
+  });
 
   return (
     <div>
       <h1>Hello</h1>
       <p>Seconds: {count}</p>
     </div>
-  )
+  );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
